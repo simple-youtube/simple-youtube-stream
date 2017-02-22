13e3cbd312a906818db977c27adaa304b0ced03d
HyperCoder <bryan.pikaardjr@outlook.com>
2017-02-22 13:59:37 -0500

    Change to module export

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,25 +1,20 @@
 const Constants = require("./Constants");
 
-class Util {
-    fetchIn(string, start, end) {
-        let startString = string.indexOf(start);
-        if (startString === -1) return "";
+exports.fetchIn = (string, start, end) => {
+    let startString = string.indexOf(start);
+    if (startString === -1) return "";
+    let slicedString = string.slice(startString + start.length);
 
-        let slicedString = string.slice(startString + start.length);
+    let endString = slicedString.indexOf(end);
+    if (endString === -1) return "";
 
-        let endString = slicedString.indexOf(end);
-        if (endString === -1) return "";
+    let newString = slicedString.slice(0, endString);
+    return newString;
+};
 
-        let newString = slicedString.slice(0, endString);
-        return newString;
-    }
-
-    buildError(code, err) {
-        let error = Constants.ERRORS[code];
-        let errorObject = { code: code, message: error };
-        if (err) Object.assign(errorObject, { ytmessage: err });
-        return errorObject;
-    }
-}
-
-module.exports = Util;
+exports.buildError = (code, err) => {
+    let error = Constants.ERRORS[code];
+    let errorObject = { code: code, message: error };
+    if (err) Object.assign(errorObject, { ytmessage: err });
+    return errorObject;
+};
